94f115b52592e796d2da48b9177935e39eee70d2
Aatesh Thirucumaran <thirucumarana@gmail.com>
2026-03-11 14:56:31 +0000

    Created skeleton file structure

---
+++ refocus/background.js
@@ -0,0 +1,3 @@
+// background.js
+// background service worker for the extension
+// detecting which website is being used, tracking time used, checking if time limit is exceeded, triggering block when limit is exceeded
---
+++ refocus/storage.js
@@ -0,0 +1,6 @@
+// storage.js
+// defines how data is read/written using chrome.storage.local
+// contains all helper functions that are used by the other files
+// data structures:
+//      - usageLog: tracks how long the user has spent on each site each day
+//      - managedSites: list of sites to be managed, and their individual settings
