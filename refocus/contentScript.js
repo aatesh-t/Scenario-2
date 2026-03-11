07c55cb1224f94509be7152a9015f93f844c8fe9
Aatesh Thirucumaran <thirucumarana@gmail.com>
2026-03-11 15:00:41 +0000

    added contentScript.js

---
+++ refocus/contentScript.js
@@ -0,0 +1,3 @@
+// contentScript.js - runs inside the actual webpage, to implement sub-path blocking
+// script will get injected when a blocked sub-path is navigated to
+// reads from managedSites from storage.js to know what to block
